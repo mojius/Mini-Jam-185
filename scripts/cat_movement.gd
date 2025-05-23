cfd39cbfeba6dc7bef850e41ed6a66cd21585d32
Alex Peña <arpenacoding@gmail.com>
2025-05-23 00:01:48 -0700

    added boundaries

--- scripts/cat_movement.gd
+++ scripts/cat_movement.gd
@@ -2,10 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var horizontal_direction := Input.get_axis("ui_left", "ui_right")
