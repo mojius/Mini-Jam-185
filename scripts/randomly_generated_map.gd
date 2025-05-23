7f0b33ca8aff45cbe18ea4383361673dfa6655a2
Ben Daly <mvben33@gmail.com>
2025-05-23 00:47:34 -0700

    Map generation works, but with no walls

--- scripts/randomly_generated_map.gd
+++ scripts/randomly_generated_map.gd
@@ -15,27 +15,24 @@ func generate():
     init_cell(rand_tile, Vector2i(0,0), visited, stack)
 
     while stack.size() > 0:
-        await get_tree().create_timer(0.08).timeout
+        await get_tree().create_timer(0.03).timeout
         var cell: Vector2i = stack.pop_front()
         print("Popped! ", cell)
         var neighbors: Array[Vector2i] = get_surrounding_cells(cell)
+        var unvisited_neighbors: Array[Vector2i]
+
         for n in neighbors:
-            if visited.get(n, false) == false and get_cell_atlas_coords(n) == Vector2i(1,1):
+            if visited.get(n, false) == false and n in get_used_cells():
                 print("Good tile at ", n)
+                unvisited_neighbors.push_front(n)
 
-                if n - cell == Vector2i.UP or n - cell == Vector2i.DOWN:
-                    init_cell(n + Vector2i.LEFT, Vector2i(1,0), visited, stack)
-                    init_cell(n + Vector2i.RIGHT, Vector2i(1,0), visited, stack)
-                else:
-                    init_cell(n + Vector2i.UP, Vector2i(1,0), visited, stack)
-                    init_cell(n + Vector2i.DOWN, Vector2i(1,0), visited, stack)
-
-
-                init_cell(n, Vector2i(0,0), visited, stack)
+        if unvisited_neighbors.size() > 0:
+            stack.push_front(cell)
+            var random_neighbor = unvisited_neighbors.pick_random()
+            init_cell(random_neighbor, Vector2i(0,0), visited, stack)
+        
 
 
-                
-            else: continue
 
 func init_cell(cell: Vector2i, atlas: Vector2i, visited: Dictionary[Vector2i, bool], stack: Array[Vector2i]):
     if cell not in get_used_cells(): return
